b77d784819e4283f32e32c946df86b8b899f8f92
hujie <380211629@qq.com>
2017-01-10 10:58:25 +0800

    JiGuangPush Practice

---
+++ app/src/main/java/com/hujie/jiguang/MainActivity.java
@@ -0,0 +1,18 @@
+package com.hujie.jiguang;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cn.jpush.android.api.JPushInterface;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        JPushInterface.setDebugMode(true);
+        JPushInterface.init(this);
+    }
+}
